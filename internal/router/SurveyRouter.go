e6085558d923262c28c3f3775751a8668cdc2b45
FatemehKeshavarzz <154620729+FatemehKeshavarzz@users.noreply.github.com>
2024-12-03 03:35:25 +0330

    Add access control middleware

--- internal/router/SurveyRouter.go
+++ internal/router/SurveyRouter.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/G9QBootcamp/qoli-survey/internal/config"
 	"github.com/G9QBootcamp/qoli-survey/internal/db"
+	middlewares "github.com/G9QBootcamp/qoli-survey/internal/middleware"
 	"github.com/G9QBootcamp/qoli-survey/internal/survey/handler"
 	"github.com/G9QBootcamp/qoli-survey/pkg/logging"
 	"github.com/labstack/echo/v4"
@@ -20,6 +21,9 @@ func NewSurveyRouter(conf *config.Config, db db.DbService, server *echo.Echo, lo
 	return &SurveyRouter{conf: conf, db: db, server: server, handler: handler.NewHandler(conf, db, logger)}
 }
 
-func (r *SurveyRouter) RegisterRoutes() {
-	r.server.POST("/surveys", r.handler.CreateSurvey)
+func (r *SurveyRouter) RegisterRoutes(db db.DbService) {
+	r.server.POST("/surveys",
+		r.handler.CreateSurvey,
+		middlewares.CheckPermission("view_survey", db), //it's an example, it should be deleted from here
+	)
 }
