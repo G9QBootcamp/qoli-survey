ab1c5127f20ba286d6358ddbdbb895bf31b77831
sajadblnyn <sajadbalaniyan.sb@gmail.com>
2024-12-07 23:32:54 +0330

    feat: add get profile api

--- internal/user/models/User.go
+++ internal/user/models/User.go
@@ -7,16 +7,19 @@ import (
 )
 
 type User struct {
-	gorm.Model
-	RoleID        uint   `gorm:"not null"`
-	NationalID    string `gorm:"unique;not null"`
-	Email         string `gorm:"unique;not null"`
-	PasswordHash  string `gorm:"not null"`
-	FirstName     string
-	LastName      string
-	DateOfBirth   time.Time
-	City          string
-	WalletBalance float64 `gorm:"default:0"`
-	EmailVerified bool    `gorm:"default:false"`
-	GlobalRole    Role    `gorm:"foreignKey:RoleID;references:ID;constraint:OnDelete:CASCADE;"`
+	ID            uint `gorm:"primarykey" json:"id"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
+	RoleID        uint           `gorm:"not null"`
+	NationalID    string         `gorm:"unique;not null" json:"national_id"`
+	Email         string         `gorm:"unique;not null" json:"email"`
+	PasswordHash  string         `gorm:"not null"`
+	FirstName     string         `json:"first_name"`
+	LastName      string         `json:"last_name"`
+	DateOfBirth   time.Time      `json:"date_of_birth"`
+	City          string         `json:"city"`
+	WalletBalance float64        `gorm:"default:0"`
+	EmailVerified bool           `gorm:"default:false"`
+	GlobalRole    Role           `gorm:"foreignKey:RoleID;references:ID;constraint:OnDelete:CASCADE;"`
 }
