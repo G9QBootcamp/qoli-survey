8464e0f07847cf287c0184fdaf039ab3a15684a1
Mahdyar <mahdyarsss80@gmail.com>
2024-12-09 19:28:45 +0330

    Survey reports

---
+++ internal/survey/repository/ReportRepository.go
@@ -0,0 +1,168 @@
+package repository
+
+import (
+	"context"
+	"errors"
+
+	"github.com/G9QBootcamp/qoli-survey/internal/survey/models"
+	userModels "github.com/G9QBootcamp/qoli-survey/internal/user/models"
+	"gorm.io/gorm"
+
+	"github.com/G9QBootcamp/qoli-survey/internal/db"
+	"github.com/G9QBootcamp/qoli-survey/pkg/logging"
+)
+
+type IReportRepository interface {
+	GetSurveyParticipantsCount(ctx context.Context, surveyId uint) (int64, error)
+	GetTotalParticipatesForSurvey(ctx context.Context, surveyId uint) (int64, error)
+	GetSurveyParticipantsCountByPermissionId(ctx context.Context, surveyId uint, permissionId uint) (int64, error)
+	GetTotalVotesToQuestionCount(ctx context.Context, qid uint) (int64, error)
+	GetSuddenlyFinishedParticipatesForSurvey(ctx context.Context, surveyId uint) (int64, error)
+	GetQuestionsBySurveyID(ctx context.Context, sid uint) ([]models.Question, error)
+	GetCorrectChoiceByQuestionID(ctx context.Context, qid uint) (*models.Choice, error)
+	GetChoicesByQuestionID(ctx context.Context, qid uint) ([]models.Choice, error)
+	GetGivenAnswerCountByQuestionID(ctx context.Context, qid uint, answer string) (int64, error)
+}
+
+type ReportRepository struct {
+	db     db.DbService
+	logger logging.Logger
+}
+
+func NewReportRepository(db db.DbService, logger logging.Logger) *ReportRepository {
+	return &ReportRepository{db: db, logger: logger}
+}
+
+func (r *ReportRepository) GetSurveyParticipantsCount(ctx context.Context, surveyId uint) (int64, error) {
+	var count int64
+	err := r.db.GetDb().WithContext(ctx).Where("survey_id = ?", surveyId).Group("user_id").Count(&count).Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "get survey participants count error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return 0, err
+	}
+	return count, nil
+}
+func (r *ReportRepository) GetSurveyParticipantsCountByPermissionId(ctx context.Context, surveyId uint, permissionId uint) (int64, error) {
+	var count int64
+	err := r.db.GetDb().WithContext(ctx).Raw("select count(usr.user_id) "+
+		"from user_survey_roles as usr "+
+		"join roles as r "+
+		"on usr.role_id = r.ID "+
+		"join role_permissions as rp "+
+		"on r.ID = rp.role_id "+
+		"where usr.survey_id = ? AND rp.permission_id = ? ",
+		surveyId,
+		permissionId).Scan(&count).Error
+	/*
+		select count(usr.user_id)
+		where usr.survey_id = ? AND rp.permission_id = ?
+		from user_survey_role as usr
+		join role as r
+		on usr.role_id = r.ID
+		join role_permission as rp
+		on r.ID = rp.role_id
+		group by usr.user_id
+
+	*/
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "GetSurveyParticipantsCountByPermissionId error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return 0, err
+	}
+	return count, nil
+}
+
+func (r *ReportRepository) GetGivenAnswerCount(ctx context.Context, ans string) (int64, error) {
+	var count int64
+	err := r.db.GetDb().WithContext(ctx).Table("votes").Where("answer = ?", ans).Count(&count).Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "GetGivenAnswerCount error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return 0, err
+	}
+	return count, nil
+}
+
+func (r *ReportRepository) GetTotalVotesToQuestionCount(ctx context.Context, qid uint) (int64, error) {
+	var count int64
+	err := r.db.GetDb().WithContext(ctx).Table("votes").Where("question_id = ?", qid).Count(&count).Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "GetTotalVotesToQuestionCount error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return 0, err
+	}
+	return count, nil
+}
+
+func (r *ReportRepository) GetTotalParticipatesForSurvey(ctx context.Context, surveyId uint) (int64, error) {
+	var count int64
+	err := r.db.GetDb().WithContext(ctx).Table("user_survey_participations").Where("survey_id = ?", surveyId).Count(&count).Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "GetTotalParticipatesForSurvey error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return 0, err
+	}
+	return count, nil
+}
+
+func (r *ReportRepository) GetSuddenlyFinishedParticipatesForSurvey(ctx context.Context, surveyId uint) (int64, error) {
+	var count int64
+	err := r.db.GetDb().WithContext(ctx).Table("user_survey_participations").
+		Where("survey_id = ? AND committed_at IS NULL AND end_at IS NOT NULL", surveyId).
+		Count(&count).
+		Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "GetSuddenlyFinishedParticipatesForSurvey error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return 0, err
+	}
+	return count, nil
+}
+
+func (r *ReportRepository) GetQuestionsBySurveyID(ctx context.Context, sid uint) ([]models.Question, error) {
+	var questions []models.Question
+	err := r.db.GetDb().WithContext(ctx).Where("survey_id = ?", sid).Find(&questions).Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "GetQuestionsBySurveyID error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return nil, err
+	}
+	return questions, nil
+}
+
+func (r *ReportRepository) GetCorrectChoiceByQuestionID(ctx context.Context, qid uint) (*models.Choice, error) {
+	var choice models.Choice
+	err := r.db.GetDb().WithContext(ctx).Where("question_id = ? AND is_correct = true", qid).First(&choice).Error
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+
+	if err != nil {
+		r.logger.Error(logging.Database, logging.Select, "GetCorrectChoiceByQuestionID error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return nil, err
+	}
+	return &choice, nil
+}
+
+func (r *ReportRepository) GetChoicesByQuestionID(ctx context.Context, qid uint) ([]models.Choice, error) {
+	var choices []models.Choice
+	err := r.db.GetDb().WithContext(ctx).Where("question_id = ?", qid).Find(&choices).Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "GetChoicesByQuestionID error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return nil, err
+	}
+	return choices, nil
+}
+
+func (r *ReportRepository) GetGivenAnswerCountByQuestionID(ctx context.Context, qid uint, answer string) (int64, error) {
+	var count int64
+	err := r.db.GetDb().WithContext(ctx).Table("votes").Where("question_id = ? AND answer = ?", qid, answer).Count(&count).Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		r.logger.Error(logging.Database, logging.Select, "GetGivenAnswerCountByQuestionID error in repository ", map[logging.ExtraKey]interface{}{logging.ErrorMessage: err.Error()})
+		return 0, err
+	}
+	return count, nil
+}
---
+++ internal/survey/service/ReportService.go
@@ -0,0 +1,166 @@
+package service
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/G9QBootcamp/qoli-survey/internal/config"
+	"github.com/G9QBootcamp/qoli-survey/internal/survey/dto"
+	"github.com/G9QBootcamp/qoli-survey/internal/survey/models"
+	"github.com/G9QBootcamp/qoli-survey/internal/survey/repository"
+	"github.com/G9QBootcamp/qoli-survey/pkg/logging"
+	"golang.org/x/net/context"
+)
+
+type IReportService interface {
+	GetTotalParticipationPercentage(ctx context.Context, surveyId uint) (uint, error)
+	GetCorrectAnswerPercentage(ctx context.Context, surveyId uint) ([]dto.CorrectAnswerPercentageToShow, error)
+	SuddenlyFinishedParticipationPercentage(ctx context.Context, surveyId uint) (float64, error)
+	GetChoicesByPercentage(ctx context.Context, surveyId uint) ([]dto.QuestionReport, error)
+}
+type ReportService struct {
+	conf   *config.Config
+	repo   repository.IReportRepository
+	logger logging.Logger
+}
+
+func NewReportService(conf *config.Config, repo repository.IReportRepository, logger logging.Logger) *ReportService {
+	return &ReportService{conf: conf, repo: repo, logger: logger}
+}
+
+func (s *ReportService) GetTotalParticipationPercentage(ctx context.Context, surveyId uint) (uint, error) {
+	participated, err := s.repo.GetTotalParticipatesForSurvey(ctx, surveyId)
+	if err != nil {
+		return 0, err
+	}
+	allowed, err := s.repo.GetSurveyParticipantsCountByPermissionId(ctx, surveyId, 1)
+	if err != nil {
+		return 0, err
+	}
+	return uint(100 * float64(participated) / float64(allowed)), nil
+}
+func (s *ReportService) GetCorrectAnswerPercentage(ctx context.Context, surveyId uint) ([]dto.CorrectAnswerPercentageToShow, error) {
+	qs, err := s.repo.GetQuestionsBySurveyID(ctx, surveyId)
+	if err != nil {
+		return nil, err
+	}
+	res := make([]dto.CorrectAnswerPercentageToShow, 0)
+	for _, q := range qs {
+		if q.HasMultipleChoice {
+			correctAns, err := s.repo.GetCorrectChoiceByQuestionID(ctx, q.ID)
+
+			if err != nil {
+				return nil, err
+			}
+			if correctAns == nil {
+				continue
+			}
+
+			totalVotesCount, err := s.repo.GetTotalVotesToQuestionCount(ctx, q.ID)
+			if err != nil {
+				return nil, err
+			}
+			if totalVotesCount == 0 {
+				res = append(res, dto.CorrectAnswerPercentageToShow{
+					QuestionID: q.ID,
+					Percentage: "0%",
+				})
+				continue
+			}
+			correctAnsCount, err := s.repo.GetGivenAnswerCountByQuestionID(ctx, q.ID, correctAns.Text)
+			if err != nil {
+				return nil, err
+			}
+			res = append(res, dto.CorrectAnswerPercentageToShow{
+				QuestionID: q.ID,
+				Percentage: strconv.Itoa(int(100*(float64(correctAnsCount)/float64(totalVotesCount)))) + "%",
+			})
+		}
+	}
+	return res, nil
+}
+
+func (s *ReportService) GetMultipleParticipationCount(ctx context.Context, surveyId uint) ([]dto.ParticipationReport, error) {
+	users, err := s.repo.GetTotalParticipants(ctx, surveyId)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]dto.ParticipationReport, 0)
+
+	for _, user := range users {
+		participationCount, err := s.repo.GetParticipationCount(ctx, surveyId, user.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		if participationCount > 1 {
+			res = append(res, dto.ParticipationReport{
+				UserID: user.ID,
+				Count:  participationCount,
+			})
+		}
+	}
+	return res, err
+}
+
+func (s *ReportService) SuddenlyFinishedParticipationPercentage(ctx context.Context, surveyId uint) (float64, error) {
+	totalParticipation, err := s.repo.GetTotalParticipatesForSurvey(ctx, surveyId)
+	if err != nil {
+		return 0, err
+	}
+	suddenlyFinished, err := s.repo.GetSuddenlyFinishedParticipatesForSurvey(ctx, surveyId)
+	if err != nil {
+		return 0, err
+	}
+	if totalParticipation == 0 {
+		return 0, nil
+	}
+	return 100 * float64(suddenlyFinished) / float64(totalParticipation), nil
+}
+
+func (s *ReportService) GetChoicesByPercentage(ctx context.Context, surveyId uint) ([]dto.QuestionReport, error) {
+	qs, err := s.repo.GetQuestionsBySurveyID(ctx, surveyId)
+	res := make([]dto.QuestionReport, 0)
+	if err != nil {
+		return nil, err
+	}
+	for _, q := range qs {
+		if q.HasMultipleChoice {
+			questionReport := dto.QuestionReport{
+				QuestionID:   q.ID,
+				ChoiceReport: make([]dto.ChoiceReport, 0),
+			}
+			choices, err := s.repo.GetChoicesByQuestionID(ctx, q.ID)
+			if err != nil {
+				return nil, err
+			}
+			totalVotesCount, err := s.repo.GetTotalVotesToQuestionCount(ctx, q.ID)
+			if err != nil {
+				return nil, err
+			}
+			for _, choice := range choices {
+				if totalVotesCount == 0 {
+					questionReport.ChoiceReport = append(questionReport.ChoiceReport, dto.ChoiceReport{
+						ID:         choice.ID,
+						Text:       choice.Text,
+						Percentage: "0 %",
+					})
+					continue
+				}
+
+				chosenCount, err := s.repo.GetGivenAnswerCountByQuestionID(ctx, q.ID, choice.Text)
+				if err != nil {
+					return nil, err
+				}
+				questionReport.ChoiceReport = append(questionReport.ChoiceReport, dto.ChoiceReport{
+					ID:         choice.ID,
+					Text:       choice.Text,
+					Percentage: strconv.Itoa(int(100*float64(chosenCount)/float64(totalVotesCount))) + "%",
+				})
+			}
+			res = append(res, questionReport)
+		}
+	}
+	return res, nil
+}
